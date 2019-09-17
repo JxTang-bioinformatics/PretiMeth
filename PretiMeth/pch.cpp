6264bb49f5713b88409fad3c02bb72f43e862693
JxTang-bioinformatics <55084319+JxTang-bioinformatics@users.noreply.github.com>
2019-09-17 10:48:19 +0800

    Delete pch.cpp

--- PretiMeth/pch.cpp
+++
@@ -1,5 +0,0 @@
-// pch.cpp: Source file corresponding to the precompiled header; required for successful compilation
-
-#include "pch.h"
-
-// Normally, this file is ignored, but if you are using a precompiled header, please keep it.
