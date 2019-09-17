7902422c20cdd4d429655350fee1295e40c83b95
JxTang-bioinformatics <55084319+JxTang-bioinformatics@users.noreply.github.com>
2019-09-17 10:37:58 +0800

    Create pch.cpp

---
+++ PretiMeth/pch.cpp
@@ -0,0 +1,5 @@
+// pch.cpp: Source file corresponding to the precompiled header; required for successful compilation
+
+#include "pch.h"
+
+// Normally, this file is ignored, but if you are using a precompiled header, please keep it.
